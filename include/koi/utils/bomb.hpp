0f79ae9201504647f82316f8dafc1f04ef86df2c
koishi <qwq@koishi.top>
2019-10-20 16:52:48 +0800

    fix c++14

--- include/koi/utils/bomb.hpp
+++ include/koi/utils/bomb.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <traits/concepts.hpp>
 
 namespace koi
@@ -11,27 +12,22 @@ namespace _
 using namespace std;
 using namespace traits::concepts;
 
-template <typename F>
 struct Bomb final : NoCopy, NoMove, NoHeap
 {
-	Bomb( F &&fn ) :
-	  fn( std::move( fn ) ) {}
+	Bomb( function<void()> &&fn ) :
+	  fn( std::move( fn ) )
+	{
+	}
 
 	~Bomb() { fn(); }
 
 private:
-	F fn;
+	function<void()> fn;
 };
-
-template <typename F>
-auto make_bomb( F &&fn )
-{
-	return Bomb( std::forward<F>( fn ) );
-}
 
 }  // namespace _
 
-using _::make_bomb;
+using _::Bomb;
 
 }  // namespace utils
 
